ddf7523f2d1a9c19a204091d972f73f268a28a19
Alberto Godoy <albertogodoy031429@gmail.com>
2023-07-18 15:22:27 -0400

    implementacion de endpoint para peticiones de onus by olt

--- app/Http/Controllers/OnuController.php
+++ app/Http/Controllers/OnuController.php
@@ -11,11 +11,11 @@ class OnuController extends Controller
     public function getData()
     {
         $data = DB::table('onus')
-                ->join('olts', 'onus.olt_id', 'olts.idOlt')
-                ->leftJoin('zones', 'onus.zone_id', 'zones.idZone')
-                ->leftJoin('odbs', 'onus.odb_id', 'odbs.idOdb')
-                ->select('onus.id', 'onus.name', 'olts.name as olt', 'onus.sn', 'zones.name as zone', 'odbs.name as odb')
-                ->get();
+            ->join('olts', 'onus.olt_id', 'olts.idOlt')
+            ->leftJoin('zones', 'onus.zone_id', 'zones.idZone')
+            ->leftJoin('odbs', 'onus.odb_id', 'odbs.idOdb')
+            ->select('onus.id', 'onus.name', 'olts.name as olt', 'onus.sn', 'zones.name as zone', 'odbs.name as odb', 'onus.vlan', 'onus.tv', 'onus.type', 'onus.onu')
+            ->get();
         return response()->json(['data' => $data], 200);
     }
 
@@ -49,6 +49,18 @@ class OnuController extends Controller
     public function destroy($id)
     {
         $data = DB::table('onus')->where('id', $id)->delete();
+        return response()->json(['data' => $data], 200);
+    }
+
+    public function showByOlt($id)
+    {
+        $data = DB::table('onus')
+            ->join('olts', 'onus.olt_id', 'olts.idOlt')
+            ->leftJoin('zones', 'onus.zone_id', 'zones.idZone')
+            ->leftJoin('odbs', 'onus.odb_id', 'odbs.idOdb')
+            ->select('onus.id', 'onus.name', 'olts.name as olt', 'onus.sn', 'zones.name as zone', 'odbs.name as odb', 'onus.vlan', 'onus.tv', 'onus.type', 'onus.onu')
+            ->where('olts.idOlt', $id)
+            ->get();
         return response()->json(['data' => $data], 200);
     }
 }
