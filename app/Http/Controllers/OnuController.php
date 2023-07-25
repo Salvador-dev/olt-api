738d7364a03c7d9c46ba9afdcdff550ced266eda
Alberto Godoy <albertogodoy031429@gmail.com>
2023-07-25 09:38:04 -0400

    se corrige peticion

--- app/Http/Controllers/OnuController.php
+++ app/Http/Controllers/OnuController.php
@@ -51,9 +51,10 @@ class OnuController extends Controller
             ->join('olts', 'onus.olt_id', 'olts.idOlt')
             ->leftJoin('zones', 'onus.zone_id', 'zones.idZone')
             ->leftJoin('odbs', 'onus.odb_id', 'odbs.idOdb')
+            ->leftJoin('speed_profiles', 'onus.speed_profile_id', 'speed_profiles.idSpeedProfile ')
             ->leftJoin('onu_types', 'onus.onu_type', 'onu_types.idOnuType')
             ->where('onus.id', $id)
-            ->select('onus.*', 'olts.name as olt', 'zones.name as zone', 'odbs.name as odb')
+            ->select('onus.*', 'olts.name as olt', 'zones.name as zone', 'odbs.name as odb', 'speed_profiles.name as speed')
             ->get();
         return response()->json(['data' => $data], 200);
     }
