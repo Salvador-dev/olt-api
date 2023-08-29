d5254397a8913b9850ad7da5363071f83f179271
Alberto Godoy <albertogodoy031429@gmail.com>
2023-08-29 16:51:19 -0400

    integracion de peticion para dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,8 +14,9 @@ class DashboardController extends Controller
         $onus = Onu::all();
         $online = $onus->where('status', 'Online')->count();
         $pending = $onus->where('administrative_status', 'Disabled')->count();
-        $signal = $onus->where('status', 'Offline')->count();
-        $data = array(['online' => $online], ['pending' => $pending], ['signal' => $signal]);
+        $offline = $onus->where('status', 'Offline')->count();
+        $signal = $onus->where('signal', '!=', 'Very good')->count();
+        $data = array(['online' => $online], ['pending' => $pending], ['offline' => $offline], ['low signal' => $signal]);
         return response()->json(['data' => $data], 200);
     }
 }
